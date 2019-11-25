9dbe95222a617fba0704e120f2f5e4aadc6489bf
jkamz <jkamz6432@gmail.com>
2019-11-25 18:49:57 +0300

    add users list model

---
+++ Codelab/Codelab/Models/ListUsersModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codelab.Models
+{
+    class ListUsersModel
+    {
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public string Image { get; set; }
+    }
+}
