1755d6146c70c29d276ef24937451f0ba17510fa
jkamz <jkamz6432@gmail.com>
2019-11-25 18:49:31 +0300

    add users list viewmodel

---
+++ Codelab/Codelab/ViewModels/ListUsersViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Codelab.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Codelab.ViewModels
+{
+    class ListUsersViewModel
+    {
+        public ObservableCollection<ListUsersModel> UsersList { get; set; }
+
+        public ListUsersViewModel()
+        {
+            UsersList = new ObservableCollection<ListUsersModel>();
+            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
+        }
+    }
+}
