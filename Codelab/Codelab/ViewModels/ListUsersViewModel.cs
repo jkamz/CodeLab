0348b3527163ad113374d83042b0fe3e899e672f
jkamz <jkamz6432@gmail.com>
2019-11-27 00:42:29 +0300

    add api calls to github

--- Codelab/Codelab/ViewModels/ListUsersViewModel.cs
+++ Codelab/Codelab/ViewModels/ListUsersViewModel.cs
@@ -1,33 +1,100 @@
 ﻿using Codelab.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Codelab.ViewModels
 {
-    class ListUsersViewModel
+    public class ListUsersViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<ListUsersModel> UsersList { get; set; }
 
-        public ListUsersViewModel()
-        {
-            UsersList = new ObservableCollection<ListUsersModel>();
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
-            UsersList.Add(new ListUsersModel { Name = "Jkamz", Image = "https://i.imgur.com/aYkGpAV.png", Detail = "The one who knocks" });
+        ObservableCollection<ListUsersModel> _usersList;
+        public ObservableCollection<ListUsersModel> UsersList
+        { 
+            get { return _usersList; } 
+            set
+            {
+                _usersList = value;
+                RaisePropertyChanged();
+            }
         }
+
+        public async Task<ApiResponse> GetUsers()
+        {
+            ApiResponse response = null;
+
+            try
+            {
+                var httpclient = new HttpClient();
+
+                httpclient.BaseAddress = new Uri("https://api.github.com/");
+                httpclient.DefaultRequestHeaders.Accept.Clear();
+                httpclient.DefaultRequestHeaders.Add("Accept", " application/json");
+
+                var httpresponse = await httpclient.GetAsync("search/users?q=location%3Akenya");
+                Stream stream = await httpresponse.Content.ReadAsStreamAsync();
+                string content = await StreamToStringAsync(stream);
+
+                response = new ApiResponse
+                {
+                    Content = content,
+                    StatusCode = (int)httpresponse.StatusCode
+                };
+
+                UsersList = new ObservableCollection<ListUsersModel>(StringToJObject(response.Content).ToObject<Models.RootObject>().items);
+
+                httpclient.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return response;
+        }
+
+        static async Task<string> StreamToStringAsync(Stream stream)
+        {
+            string content = null;
+
+            if (stream != null)
+                using (var sr = new StreamReader(stream))
+                    content = await sr.ReadToEndAsync();
+
+            return content;
+        }
+
+        public static JObject StringToJObject(string jsonString)
+        {
+            JObject data = null;
+
+            try
+            {
+                data = JObject.Parse(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return data;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
