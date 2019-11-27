3dfd5f25de9d58604d79ba5c56625dee0690d514
jkamz <jkamz6432@gmail.com>
2019-11-27 17:50:01 +0300

    add user profile model

---
+++ Codelab/Codelab/Models/UserProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codelab.Models
+{
+    public class UserProfileModel
+    {
+        public string name { get; set; }
+        public int public_repos { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+    }
+}
