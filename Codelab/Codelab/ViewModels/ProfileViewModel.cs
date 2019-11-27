5eacfac0b003065bd374c799bd7f3b70936a8010
jkamz <jkamz6432@gmail.com>
2019-11-27 17:50:30 +0300

    add user profile view model

---
+++ Codelab/Codelab/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Codelab.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codelab.ViewModels
+{
+    public class ProfileViewModel
+    {
+        static public UserProfileModel UserProfile;
+
+        static public async Task<ApiResponse> GetUser()
+        {
+            ApiResponse response = null;
+            HttpClient httpClient = new HttpClient();
+            var httpresponse = await httpClient.GetStringAsync("https://api.github.com/users/kenyansongithub");
+            var user = JsonConvert.DeserializeObject<UserProfileModel>(httpresponse);
+            response = new ApiResponse
+            {
+                Content = httpresponse,
+                StatusCode = 200
+            };
+
+            UserProfile = user;
+
+            return response;
+        }
+    }
+}
